fb2343ebb9cf2cfa7ccc48a85e151f7858899e2b
Siva Chintakana <siva@cliqr.com>
2016-01-11 15:18:53 -0800

    adding markdown plugins

--- apm-install.sh
+++ apm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install atom plugins
-for i in minimap linter language-puppet git-plus vim-mode
+for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf
 do
   apm install $i
 done
