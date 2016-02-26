2111bdd315a34c17b9c11341e848c8c03a0db1f4
Siva Chintakana <siva@cliqr.com>
2016-02-26 11:53:05 -0800

    added script plugin

--- apm-install.sh
+++ apm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install atom plugins
-for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml
+for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script
 do
   apm install $i
 done
