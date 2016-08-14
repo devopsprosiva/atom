f2df121dd35d98b0262bb3de15299e5a9b524fbb
DevOpsPro <devopsprosiva@gmail.com>
2016-08-14 02:10:39 -0700

    added script plugin

--- apm-install.sh
+++ apm-install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Install hydrogen (mac)
+brew install pkg-config
+brew install zeromq
+sudo pip install jupyter
+
+
+
 # Install atom plugins
-for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python
+for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script
 do
   apm install $i
 done
