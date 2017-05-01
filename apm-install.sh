33cae81bd25ffa05a757e44a95b4f6de8cbccb4f
devopsprosiva <devopsprosiva@users.noreply.github.com>
2017-05-01 15:05:41 -0700

    added atom-jinja2

--- apm-install.sh
+++ apm-install.sh
@@ -8,7 +8,7 @@ sudo pip install jupyter
 
 
 # Install atom plugins
-for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script pretty-json language-terraform linter-terraform-syntax
+for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script pretty-json language-terraform linter-terraform-syntax atom-jinja2
 do
   apm install $i
 done
