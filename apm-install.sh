905b1f660682effaac8c8dbd5fdf48622959554c
devopsprosiva <devopsprosiva@users.noreply.github.com>
2017-05-29 19:52:27 -0700

    Update apm-install.sh
    busy-signal
    intentions

--- apm-install.sh
+++ apm-install.sh
@@ -8,7 +8,7 @@ sudo pip install jupyter
 
 
 # Install atom plugins
-for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script pretty-json language-terraform linter-terraform-syntax atom-jinja2 language-groovy atom-beautify linter-ui-default
+for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script pretty-json language-terraform linter-terraform-syntax atom-jinja2 language-groovy atom-beautify linter-ui-default intentions busy-signal
 do
   apm install $i
 done
