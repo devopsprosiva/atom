e0edc530c7b419537ba608753ff65c95fdb7f4d5
devopsprosiva <devopsprosiva@users.noreply.github.com>
2018-11-22 18:37:52 -0800

    removed markdown-scroll-sync

--- apm-install.sh
+++ apm-install.sh
@@ -8,7 +8,7 @@
 
 
 # Install atom plugins
-for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf markdown-scroll-sync scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script pretty-json language-terraform linter-terraform-syntax atom-jinja2 language-groovy atom-beautify linter-ui-default intentions busy-signal
+for i in minimap linter language-puppet git-plus vim-mode markdown-writer markdown-preview-plus markdown-pdf scroll-sync linter-js-yaml script platformio-ide-terminal autocomplete-python hydrogen script pretty-json language-terraform linter-terraform-syntax atom-jinja2 language-groovy atom-beautify linter-ui-default intentions busy-signal
 do
   apm install $i
 done
